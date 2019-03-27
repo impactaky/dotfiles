c66a91cdf5cd5a987322ece232212b5c46cea00b
impactaky <impactaky@users.noreply.github.com>
2019-03-27 23:45:20 +0900

    Add install clang

--- rtags/setup.sh
+++ rtags/setup.sh
@@ -10,8 +10,8 @@ git clone --recursive https://github.com/Andersbakken/rtags.git
 cd rtags
 mkdir ./build
 cd ./build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
-make -j$(cat /proc/cpuinfo| grep processor | wc -l)
+cmake -DCMAKE_CXX_COMPILER="clang++-7" -DCMAKE_C_COMPILER="clang-7" ..
+ninja
 sudo porg -lp rtags make install
 
 systemctl --user enable rdm.socket
--- ubuntu1804.sh
+++ ubuntu1804.sh
@@ -7,3 +7,6 @@ sudo apt install -y \
 	porg golang zsh gawk curl trash-cli automake 
 sudo pip3 install neovim
 sudo pip3 install xonsh pexpect
+
+sudo apt install clang-7 clang-format-7 clang-tidy-7 \
+    libclang-7-dev lld-7 lldb-7 llvm-7-dev ninja-build

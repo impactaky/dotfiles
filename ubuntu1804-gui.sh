bb2f99537ccd9dc048d1b706566b9f0e2da878bd
impactaky <impactaky@users.noreply.github.com>
2020-10-15 22:26:17 +0900

    Update google chrome install script

--- ubuntu1804-gui.sh
+++ ubuntu1804-gui.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install -y gnome-session vanilla-gnome-desktop google-chrome-stable
 sudo pip3 install selenium chromedriver-binary

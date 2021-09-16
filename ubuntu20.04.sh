67db1d22d7d3741d1e53f06b79db6bf1d1fcc9f3
impactaky <impactaky@users.noreply.github.com>
2021-09-16 23:57:14 +0900

    Remove old ppa

--- ubuntu20.04.sh
+++ ubuntu20.04.sh
@@ -11,11 +11,6 @@ sudo pip3 install cmake
 sudo pip3 install pynvim
 sudo pip3 install xonsh pexpect python-language-server
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-echo deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main \
-   | sudo tee -a /etc/apt/sources.list
-echo deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main \
-   | sudo tee -a /etc/apt/sources.list
 sudo apt update
 sudo apt install clang-12 clang-format-12 clang-tidy-12 clang-tools-12 clangd-12 \
     libclang-12-dev lld-12

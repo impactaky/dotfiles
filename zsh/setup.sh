a5b8ba03c6d7d2607299d0d4d3200e303bb2df51
impactaky <impactaky@users.noreply.github.com>
2021-11-20 14:59:12 +0900

    Update zinit url

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,7 +3,7 @@
 DOT_DIR=${DOT_DIR:-$HOME/dotfiles}
 
 # zinit
-git clone https://github.com/zdharma/zinit.git $HOME/.zinit/bin
+git clone https://github.com/zdharma-continuum/zinit $HOME/.zinit/bin
 
 # zsh
 ln -s $DOT_DIR/zsh/.zshrc $HOME/.zshrc

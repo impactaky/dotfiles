536c67341cf1174e65a47fa64d70b7db58d0dab2
impactaky <impactaky@users.noreply.github.com>
2023-04-09 16:32:22 +0900

    Update install script

--- ubuntu20.04.sh
+++ ubuntu20.04.sh
@@ -1,16 +1,34 @@
-#!/bin/bash -eu
+#!/bin/bash
+
+set -eux
+
+CLANG_VERSION=15
+IS_SERVER=${IS_SERVER:-no}
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo sh -c "wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -"
+sudo sh -c 'echo "deb http://apt.llvm.org/$(lsb_release -sc)/ llvm-toolchain-$(lsb_release -sc)-15 main" >> /etc/apt/sources.list'
 sudo apt update
 sudo apt install -y \
 	git neovim python3 python3-pip \
-	tmux libudev-dev libusb-1.0-0-dev libboost-dev libboost-regex-dev \
-	porg golang zsh gawk curl trash-cli automake ninja-build ccache
+	tmux porg golang zsh gawk curl trash-cli automake ninja-build ccache \
+    clang-"${CLANG_VERSION}" \
+    clang-format-"${CLANG_VERSION}" \
+    clang-tidy-"${CLANG_VERSION}" \
+    clang-tools-"${CLANG_VERSION}" \
+    clangd-"${CLANG_VERSION}" \
+    libclang-"${CLANG_VERSION}"-dev \
+    lld-"${CLANG_VERSION}"
+
 sudo pip3 install -U pip
 sudo pip3 install cmake
 sudo pip3 install pynvim
-sudo pip3 install xonsh pexpect python-language-server
+sudo pip3 install pexpect python-language-server
 
-sudo apt update
-sudo apt install clang-12 clang-format-12 clang-tidy-12 clang-tools-12 clangd-12 \
-    libclang-12-dev lld-12
+
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+if [[ "${IS_SERVER}" != "no" ]]; then
+    sudo apt install -y \
+        libudev-dev libusb-1.0-0-dev libboost-dev libboost-regex-dev
+fi

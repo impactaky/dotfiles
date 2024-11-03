e9387e6fc4419736fae23f1e1b806e6296a6b1eb
impactaky <impactaky@users.noreply.github.com>
2024-11-03 21:00:50 +0900

    Remove old ubuntu

--- ubuntu1804-gui.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt update
-sudo apt install -y gnome-session vanilla-gnome-desktop google-chrome-stable
-sudo pip3 install selenium chromedriver-binary

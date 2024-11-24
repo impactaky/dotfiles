071fd3d69fd8a104223e36c7ae56594de924aa63
impactaly <impactaky@users.noreply.github.com>
2024-11-24 19:03:36 +0900

    Work without nix-homemanager

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "dotfile requirements";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+     self,
+     nixpkgs,
+  }: let
+		system = "aarch64-linux";
+	in {
+    packages.${system}.default = nixpkgs.legacyPackages.${system}.buildEnv {
+      name = "impactaky-dotfiles";
+      paths = with nixpkgs.legacyPackages.${system}; [
+        gh
+        git
+        neovim
+        nushell
+        ripgrep
+        starship
+        tmux
+        uv
+      ];
+    };
+  };
+}

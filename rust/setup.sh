5cbb08cacf628dead919ec6a3202cb1ecdc5c8a9
impactaly <impactaky@users.noreply.github.com>
2025-01-04 21:34:38 +0900

    Remove rust instal script

--- rust/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup install nightly
-rustup override set nightly
-rustup default nightly
-rustup component add rust-analyzer-preview
-cargo install cargo-edit

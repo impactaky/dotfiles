1029529a23c68878df8a66ee70abcc9d9967fcfd
impactaly <impactaky@users.noreply.github.com>
2025-01-05 21:49:10 +0900

    Support multiarch

--- flake.nix
+++ flake.nix
@@ -4,31 +4,33 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs, }:
-    let system = "aarch64-linux";
+    let systems = [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" ];
     in {
-      packages.${system}.default = nixpkgs.legacyPackages.${system}.buildEnv {
-        name = "impactaky-dotfiles";
-        paths = with nixpkgs.legacyPackages.${system}; [
-          atuin
-          deno
-          fzf
-          gh
-          git
-          httpie
-          lazydocker
-          neovim
-          nufmt
-          nushell
-          nixfmt
-          ripgrep
-          starship
-          thefuck
-          tmux
-          uv
-          zinit
-          zoxide
-          zsh
-        ];
-      };
+      packages = nixpkgs.lib.genAttrs systems (system: {
+        default = nixpkgs.legacyPackages.${system}.buildEnv {
+          name = "impactaky-dotfiles";
+          paths = with nixpkgs.legacyPackages.${system}; [
+            atuin
+            deno
+            fzf
+            gh
+            git
+            httpie
+            lazydocker
+            neovim
+            nufmt
+            nushell
+            nixfmt
+            ripgrep
+            starship
+            thefuck
+            tmux
+            uv
+            zinit
+            zoxide
+            zsh
+          ];
+        };
+      });
     };
 }

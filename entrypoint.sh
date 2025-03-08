5fbd022304d566f2cec0e334116430abee1afe09
impactaly <impactaky@users.noreply.github.com>
2025-03-08 22:05:04 +0900

    Always load env.sh regardless of arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
+. "$SCRIPT_DIR/env.sh"
+
 if [ $# -gt 0 ]; then
     exec "$@"
 else
-    SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
-    . "$SCRIPT_DIR/env.sh"
     exec zsh
 fi
